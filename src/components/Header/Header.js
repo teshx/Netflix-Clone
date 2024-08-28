9c3daf3ee70697bb8026faaa679854d3385aeb15
teshager habtie <teshie00001@gmail.com>
2024-08-28 00:08:52 -0500

    this is Header componnent

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,1 +1,53 @@
 
+import React from "react";
+// import logo from "../../assets/images/netflix_Logos.jpg";
+import logos from "../../assets/images/file1.png";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+
+function Header() {
+  return (
+    <>
+      <div class="Header_outer_container">
+        <div class="Header_container">
+          <div class="Header_left">
+            <ul>
+              <li>
+                <img className="logos" src={logos} alt="Netflix-lofo" />
+              </li>
+              <li>Home</li>
+              <li>TvShow</li>
+              <li>Movies</li>
+              <li>Latest</li>
+              <li>MyList</li>
+              <li>Browser by Language</li>
+            </ul>
+          </div>
+          <div class="Header-right">
+            <ul>
+              <li>
+                <SearchIcon />
+              </li>
+              <li>
+                <NotificationsNoneIcon />
+              </li>
+              <li>
+                {" "}
+                <AccountBoxIcon />{" "}
+              </li>
+              <li>
+                {" "}
+                <ArrowDropDownIcon />{" "}
+              </li>
+              <li></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
