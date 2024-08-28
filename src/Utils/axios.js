594b6fcd80856602f823ee742fb890e3ce784112
teshager habtie <teshie00001@gmail.com>
2024-08-28 04:58:57 -0500

    this the request url,.env file and axioss

--- src/Utils/axios.js
+++ src/Utils/axios.js
@@ -1,1 +1,6 @@
 import axios from "axios";
+
+const instance = axios.create({ baseURL: "https://api.themoviedb.org/3" });
+
+export default instance;
+// https://api.themoviedb.org/3/trending/all/week?api_key=7c20fcfd19c65e00fab771ebcf54fd8f&language-en=US
